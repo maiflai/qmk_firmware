856cb8657fd08de50c7b29891729b1b8d6949a97
Stu <maiflai@users.noreply.github.com>
2017-12-14 20:30:43 +0000

    add INS

--- keyboards/iris/keymaps/maiflai/keymap.c
+++ keyboards/iris/keymaps/maiflai/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-------+-------+-------+-------+-------+-------.                            ,-------+-------+-------+-------+-------+-------.
      LALT(KC_PSCREEN),KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                    KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,
   //|-------+-------+-------+-------+-------+-------|                            |-------+-------+-------+-------+-------+-------|
-     _______,_______, UK_AT ,KC_LBRC,KC_RBRC,KC_GRV ,                             KC_NUBS,KC_PGUP,KC_PGDN,KC_HOME,KC_END ,_______,
+     _______,_______, UK_AT ,KC_LBRC,KC_RBRC,KC_GRV ,                             KC_NUBS,KC_PGUP,KC_PGDN,KC_HOME,KC_END ,KC_INS ,
   //|-------+-------+-------+-------+-------+-------|                            |-------+-------+-------+-------+-------+-------|
      _______,KC_LCBR,KC_RCBR,KC_LPRN,KC_RPRN,KC_NUHS,                             KC_QUOT,KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,_______,
   //|-------+-------+-------+-------+-------+-------|                            |-------+-------+-------+-------+-------+-------|
