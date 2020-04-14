f3f0144e6c6f359aa1ce05a7ad7c9734c658df82
Stu <maiflai@users.noreply.github.com>
2020-04-14 20:39:46 +0100

    add volume keys to ergodox

--- keyboards/ergodox_ez/keymaps/maiflai/keymap.c
+++ keyboards/ergodox_ez/keymaps/maiflai/keymap.c
@@ -21,19 +21,19 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   ____,
+        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   EPRM,
         KC_TAB,         KC_Q,         KC_W,   KC_F,   KC_P,   KC_G,   ____,
         MO(FN),         KC_A,         KC_R,   KC_S,   KC_T,   KC_D,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ____,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_MUTE,
         ____,           ____,         KC_LGUI,KC_LALT,CTL_T(KC_EQL),
                                                                       KC_NUHS,  KC_INS,
                                                                                 KC_HOME,
                                                             KC_SPC, KC_EQL,     KC_END,
         // right hand
         ____,           KC_6,     KC_7,    KC_8,   KC_9,   KC_0,             ____,
-        ____,           KC_J,     KC_L,    KC_U,   KC_Y,   KC_SCLN,          KC_DEL,
+        KC_VOLU,           KC_J,     KC_L,    KC_U,   KC_Y,   KC_SCLN,          KC_DEL,
                         KC_H,     KC_N,    KC_E,   KC_I,   KC_O,             KC_BSPC,
-        ____,           KC_K,     KC_M,    KC_COMM,KC_DOT, KC_SLSH,          KC_SUPR,
+        KC_VOLD,           KC_K,     KC_M,    KC_COMM,KC_DOT, KC_SLSH,          KC_SUPR,
 	                             MO(META),MO(FN),KC_RALT,____  ,          ____,
         KC_DEL ,KC_RGUI,
         KC_PGUP,
@@ -41,20 +41,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 [FN] = LAYOUT_ergodox(
        // left hand
-       ____, KC_F1,  KC_F2,   KC_F3,   KC_F4,  KC_F5,  ____,
-       ____,  ____,  ____,    KC_UNDS, ____,   ____,   ____,
-       ____,  ____,  ____,    KC_MINS, ____,   ____,
-       ____,  ____,  ____,    ____,    ____,   ____,   ____,
-       ____,  ____,  ____,    ____,    ____,
+       KC_F12, KC_F1,  KC_F2,   KC_F3,   KC_F4,  KC_F5,  ____,
+       ____,   ____,  ____,    KC_UNDS, ____,   ____,   ____,
+       ____,   ____,  ____,    KC_MINS, ____,   ____,
+       ____,   ____,  ____,    ____,    ____,   ____,   ____,
+       ____,   ____,  ____,    ____,    ____,
                                        ____,____,
                                                ____,
                                ____,____,____,
        // right hand
-       ____, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F12,
+       ____, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
        ____, ____,    ____,   KC_PLUS, ____,    ____,    ____,
              ____,    ____,   KC_EQL,  ____,    ____,    ____,
        ____, ____,    ____,   ____,    ____,    ____,    ____,
-                      ____,  ____,    ____,    ____,    ____,
+                      ____,   ____,    ____,    ____,     ____,
        ____, ____,
        ____,
        ____, ____, ____
