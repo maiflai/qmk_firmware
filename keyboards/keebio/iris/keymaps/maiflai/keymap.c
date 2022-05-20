84d7a4a24a28b16e9f67b0a6155eee1d93f00b05
Stu <maiflai@users.noreply.github.com>
2022-05-20 21:55:25 +0100

    office keyboard is US layout

--- keyboards/keebio/iris/keymaps/maiflai/keymap.c
+++ keyboards/keebio/iris/keymaps/maiflai/keymap.c
@@ -1,136 +1,71 @@
 #include "iris.h"
-#include "action_layer.h"
-#include "eeconfig.h"
-#include "keymap_uk.h"
 
 extern keymap_config_t keymap_config;
 
 #define _COLEMAK 0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 16
 
 enum custom_keycodes {
   COLEMAK = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST,
 };
 
 #define KC_ KC_TRNS
 #define _______ KC_TRNS
 
-#define KC_LOWR LOWER
-#define KC_RASE RAISE
-#define KC_RST RESET
-#define KC_BL_S BL_STEP
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
 
 #define KC_PSCN LALT(KC_PSCREEN)
-#define KC_SUPR RCTL(KC_RSFT)
+#define SUPER RCTL(KC_RSFT)
 #define KC_LGON LCTL(LGUI(LALT(KC_DEL)))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_COLEMAK] = LAYOUT_kc(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     ESC , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,RGUI,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     TAB , Q  , W  , F  , P  , G  ,                J  , L  , U  , Y  ,SCLN,DEL ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     LCTL, A  , R  , S  , T  , D  ,                H  , N  , E  , I  , O  ,BSPC,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     LSFT, Z  , X  , C  , V  , B  ,    ,         , K  , M  ,COMM,DOT ,SLSH,SUPR,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       LALT,LGUI,SPC,          ENT,LOWR,RASE
-  //                  `----+----+----'        `----+----+----'
+  [_COLEMAK] = LAYOUT(
+  //,----+----+----+----+----+----.                                 ,----+----+----+----+----+----.
+     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_RGUI,
+  //|----+----+----+----+----+----|                                 |----+----+----+----+----+----|
+     KC_TAB , KC_Q  , KC_W  , KC_F  , KC_P  , KC_G  ,                KC_J  , KC_L  , KC_U  , KC_Y  ,KC_SCLN,KC_DEL ,
+  //|----+----+----+----+----+----|                                 |----+----+----+----+----+----|
+     KC_LGUI, KC_A  , KC_R  , KC_S  , KC_T  , KC_D  ,                KC_H  , KC_N  , KC_E  , KC_I  , KC_O  ,KC_BSPC,
+  //|----+----+----+----+----+----+----.                            ,----|----+----+----+----+----+----|
+     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  , KC_   ,  KC_       , KC_K  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,SUPER,
+  //`----+----+----+--+-+----+----+----/    i                       \----+----+----+----+----+----+----'
+                       KC_LALT,KC_LCTL,KC_SPC,                       KC_ENT,LOWER,RAISE
+  //                  `----+----+----'                                `----+----+----'
   ),
 
   [_LOWER] = LAYOUT(
   //,-------+-------+-------+-------+-------+-------.                            ,-------+-------+-------+-------+-------+-------.
      KC_PSCN,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                       KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,
   //|-------+-------+-------+-------+-------+-------|                            |-------+-------+-------+-------+-------+-------|
-     _______,LCTL(KC_Z), KC_EQL ,KC_LBRC,KC_RBRC,KC_GRV ,                          KC_NUBS,KC_HOME, KC_UP ,KC_END ,KC_PGUP,KC_INS ,
+     _______,LCTL(KC_Z), KC_EQL ,KC_LBRC,KC_RBRC,KC_GRV ,                          LSFT(KC_HASH),KC_HOME, KC_UP ,KC_END ,KC_PGUP,KC_INS ,
   //|-------+-------+-------+-------+-------+-------|                            |-------+-------+-------+-------+-------+-------|
-     _______,KC_LCBR,KC_RCBR,KC_LPRN,KC_RPRN,KC_NUHS,                             KC_QUOT,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,_______,
+     _______,KC_LCBR,KC_RCBR,KC_LPRN,KC_RPRN,KC_BSLS,                             KC_QUOT,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,_______,
   //|-------+-------+-------+------+-------+-------|                            |-------+-------+-------+-------+-------+-------|
-     _______,KC_EXLM,UK_QUOT,UK_PND ,KC_DLR ,KC_PERC,_______,             _______,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,
+     _______,KC_EXLM,KC_QUOT,KC_HASH ,KC_DLR ,KC_PERC,_______,             _______,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,
   //`-------+-------+-------+-------+-------+-------+-------+----/  \----+-------+-------+-------+-------+-------+-------+-------'
                                      _______,_______,KC_EQL,             KC_MINS,_______,_______
   //                                `-------+-------+-------'             `------+-------+-------'
   ),
 
-  [_RAISE] = LAYOUT_kc(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     F12 , F1 , F2 , F3 , F4 , F5 ,                F6 , F7 , F8 , F9 ,F10 ,F11 ,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,    ,    ,UNDS,    ,    ,                   ,    ,PLUS,    ,    ,LGON,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,    ,    ,MINS,    ,    ,               MUTE,    ,EQL ,    ,VOLU,    ,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-         ,    ,    ,    ,    ,    ,    ,         ,    ,MRWD,MPLY,MFFD,VOLD,    ,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                           ,      ,   ,          ,    ,
-  //                  `----+----+----'        `----+----+----'
+  [_RAISE] = LAYOUT(
+  //,----+----+----+----+----+----.                                              ,----+----+----+----+----+----.
+     KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                KC_F6 , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
+  //|----+----+----+----+----+----|                                              |----+----+----+----+----+----|
+     KC_ , KC_ , KC_  ,KC_UNDS, KC_  , KC_  ,               KC_   , KC_  ,KC_PLUS, KC_  , KC_  ,KC_LGON,
+  //|----+----+----+----+----+----|                                              |----+----+----+----+----+----|
+     KC_   , KC_  , KC_  ,KC_MINS,KC_   , KC_  ,               KC_MUTE, KC_  ,KC_EQL , KC_  ,KC_VOLU,KC_   ,
+  //|----+----+----+----+----+----+----.                                    ,----|----+----+----+----+----+----|
+     KC_   , KC_  , KC_  , KC_  , KC_  ,  KC_ , KC_  , KC_       , KC_  ,KC_MRWD,KC_MPLY,KC_MFFD,KC_VOLD,KC_   ,
+  //`----+----+----+--+-+----+----+----/                                    \----+----+----+----+----+----+----'
+                       KC_   ,   KC_  ,KC_  ,       KC_  , KC_  , KC_
+  //                  `----+----+----'                                        `----+----+----'
   ),
 
-  [_ADJUST] = LAYOUT(
-  //,--------+--------+--------+--------+--------+--------.                          ,--------+--------+--------+--------+--------+--------.
-      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      RESET  , DEBUG  , _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------+--------.        ,--------|--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
-  //`--------+--------+--------+----+---+--------+--------+--------/        \--------+--------+--------+---+----+--------+--------+--------'
-                                      _______, _______, _______,                  _______, _______, _______
-  //                                `--------+--------+--------'                `--------+--------+--------'
-  )
 
 };
 
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
-// Holding both LOWER and RAISE at the same time will turn on ADJUST.
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case COLEMAK:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_COLEMAK);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-      break;
-  }
-  return true;
-}
