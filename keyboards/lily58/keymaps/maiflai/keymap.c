a7464dbd0cca9269ed3f144213d3c9ccbfc6940a
Stu <maiflai@users.noreply.github.com>
2025-01-10 15:44:51 +0000

    swap LCTL and LGUI for lily to match iris

--- keyboards/lily58/keymaps/maiflai/keymap.c
+++ keyboards/lily58/keymaps/maiflai/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-C-----+-------+-------+-------+-------+-------+-------.    ,-------|-------+-------+-------+-------+-------+-------|
      KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,MC_VOLU     , MC_VOLD,KC_K  , KC_M  , KC_COMM,KC_DOT ,KC_SLSH,KC_SUPR,
   //`-------+-------+-------+--+-+-------+-------+-------/       \-------+-------+-------+-------+-------+------+-------'
-                              KC_LCTL,KC_LALT,KC_LGUI,KC_SPC      , KC_ENT,KC_LOWR,KC_RASE, KC_
+                              KC_LGUI,KC_LALT,KC_LCTL,KC_SPC      , KC_ENT,KC_LOWR,KC_RASE, KC_
   //                           `-------+-------+-------'           `-------+-------+-------'
   ),
 
